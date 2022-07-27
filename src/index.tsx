f8d1b439f80da620ab9f18f03c1b171e479198df
Kevin Smith <kevinsmithwebdev@gmail.com>
2022-07-27 15:28:01 +0200

    work

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { createBrowserHistory } from 'history'
 
 import './index.css'
 import App from './App'
@@ -9,13 +10,34 @@ import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import store from './state/store'
 
+const history = createBrowserHistory()
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+if (root) {
+  // eslint-disable-next-line no-restricted-globals
+  const path = (/#!(\/.*)$/.exec(location.hash) || [])[1]
+
+  if (path) {
+    history.replace(path)
+  }
+
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  )
+}
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
