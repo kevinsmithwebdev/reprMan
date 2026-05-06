16adbf9bd06dfecc2b5e230a370b14eba3c647e8
Kevin Smith <kevinsmithwebdev@gmail.com>
2026-05-06 14:17:27 +0200

    update About text

--- apps/client-web/src/pages/About/About.tsx
+++ apps/client-web/src/pages/About/About.tsx
@@ -26,7 +26,6 @@ const About = () => {
       <ul>
         <li>accounts</li>
         <li>mobile versions</li>
-        <li>remote storage - all versions share the same list</li>
         <li>sharing</li>
         <li>similar feature for things like scales, etc.</li>
         <li>dark mode</li>
